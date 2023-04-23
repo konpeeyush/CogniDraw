d07cf0a521d19975713fdd3b0386696a7721f38b
konpeeyush <piyushksingh2589@gmail.com>
2023-04-23 10:37:43 +0530

    Adding Changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,17 +13,22 @@ const CreatePosts = () => {
         photo: '',
     });
 
-const handleSubmit = () => {
+    const generateImage = () => {
+        
+    }
 
-}
+    const handleSubmit = () => {
 
-const handleChange = (e) => {
+    }
 
-}
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
 
-const handleSurpriseMe=()=>{
-    
-}
+    const handleSurpriseMe = () => {
+        const randomPrompt = getRandomPrompt()
+        setForm({ ...form, prompt: randomPrompt })
+    }
 
     const [generatingImg, setGeneratingImg] = useState(false)
     const [loading, setLoading] = useState(false)
@@ -38,7 +43,7 @@ const handleSurpriseMe=()=>{
             <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-5'>
                     <FormField
-                        labelName="Your Name"
+                        labelName='Your Name'
                         type="text"
                         name="name"
                         placeholder="John Doe"
@@ -52,8 +57,50 @@ const handleSurpriseMe=()=>{
                         placeholder="A man standing in front of a stargate to another dimension"
                         value={form.prompt}
                         handleChange={handleChange}
+                        isSurpriseMe
                         handleSurpriseMe={handleSurpriseMe}
                     />
+                    <div className='relative bg-gray-50 border border-gray-300 text-gray-900-sm rounded-lg focus:ring-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+                        {form.photo ? (
+                            <img
+                                src={form.photo}
+                                alt={form.prompt}
+                                className='w-full h-full object-contain'
+                            />
+                        ) :
+                            (<img
+                                src={preview}
+                                alt='preview'
+                                className='w-9/12 h-9/12 object-contain opacity-40'
+                            />)
+                        }
+
+                        {generatingImg && (
+                            <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                                <Loader />
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <div className='mt-5 flex gap-5'>
+                    <button
+                        type='button'
+                        onClick={generateImage}
+                        className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+                    >
+                        {generatingImg ? 'Generate...' : 'Generate'}
+                    </button>
+                </div>
+
+                <div className='mt-10'>
+                    <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community</p>
+                    <button
+                        type='submit'
+                        className='mt-3 text-white bg-[#6449ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+                    >
+                        {loading ? 'Sharing...' : 'Share with the community'}
+                    </button>
                 </div>
             </form>
         </section>
---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+
+dotenv.config();
